af1ccab1ee5e590e8b846b53a0fdc3c1fa3de6a3
Pau Villén <pauvillenvidal@gmail.com>
2026-03-25 14:52:50 +0100

    Fix redirects

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,19 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.exceptions import NotFound
 from app import create_app
+from flask import Flask
 
 original_app = create_app()
-
 PREFIX = 'flowcyto'
 
-from flask import Flask
 hostedApp = Flask(__name__)
 hostedApp.wsgi_app = DispatcherMiddleware(NotFound(), {
     f"/{PREFIX}": original_app
 })
 
-app = hostedApp
+def proxy_fix_app(environ, start_response):
+    environ['HTTP_HOST'] = 'formacio.bq.ub.edu'
+    environ['wsgi.url_scheme'] = 'https'
+    return hostedApp(environ, start_response)
 
-if __name__ == "__main__":
-    app.run()
+app = proxy_fix_app
